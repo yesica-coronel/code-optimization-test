cf9fa70628c63d197aefb9f55ee87361594eba5e
yesica <ypcoronelm@ufpso.edu.co>
2024-09-14 07:52:55 -0500

    refactor complete for quiz

--- src/Store.java
+++ src/Store.java
@@ -1,8 +1,19 @@
+
 public class Store {
+    //definimos constantes
+    public static final int[] PRODUCT_PRICES =(15, 10, 5);
+    private static final int[] PRODUCT_QUANTITIES = (2, 3, 4);
+    private static final int[] SALES_THRESHOLD = 50;
     public static void main(String[] args) {
+        //SI LOS ARRAY NO TIENEN LA MISMA LONGITUD
+        if (PRODUCT_PRICES.length!= PRODUCT_QUANTITIES.length);
+        System.out.println(error);
         int p1 = 15;
         int p2 = 10;
         int p3 = 5;
+        //creamos un contador para el total
+        int totalSales= 0;
+        
 
         int total1 = p1 * 2;
         int total2 = p2 * 3;
